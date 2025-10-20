02a745178eccdf62076c20bebad007b45ccafee8
alexnevess <alexsndsf@gmail.com>
2025-10-20 15:45:48 -0300

    Conclusão do model Cliente

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    //
+    protected $fillable = [
+        'empresa_id',
+        'nome',
+        'telefone',
+        'email',
+        'cnpj_cpf',
+    ];
+
+    //Um cliente pertence a uma empresa
+    public function empresa()
+    {
+        return $this->belongsTo(Empresa::class);
+    }
+
+    //Um Cliente pode ter muitos equipamentos
+    public function equipamento()
+    {
+        return $this->hasmany(Equipamento::class);
+    }
+
+    //Um Cliente pode ter muitas ordens de serviçod
+    public function ordensServico()
+    {
+        return $this->hasmany(OrdemServico::Class);
+    }
 }
