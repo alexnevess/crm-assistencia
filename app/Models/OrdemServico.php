b91ebf5cce7e2b6fa65e4fe37dbc8599e8e87cca
alexnevess <alexsndsf@gmail.com>
2025-10-20 16:01:01 -0300

    Conclusão do model OrdemServico e adição do hasfactory em todos os models

--- app/Models/OrdemServico.php
+++ app/Models/OrdemServico.php
@@ -2,9 +2,62 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OrdemServico extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'ordens_servico'; // Garante a conexão com a tabela
+
+    protected $fillable = [
+        //Chaves de vínculo
+        'empresa_id',
+        'cliente_id',
+        'equipamento_id',
+        'criador_user_id',
+        'atualizado_por_user_id',
+
+        //Dados da OS
+        'status',
+        'numero',
+        'problema_relato',
+        'diagnostico',
+        'resolucao',
+        'prazo_previsto',
+        'finalizado_em',
+    ];
+
+    //Uma OS pertence a uma empresa
+    public function empresa()
+    {
+        return $this->belongsTo(Empresa::class);
+    }
+
+    //Uma OS pertence a um cliente
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
+
+    //Uma OS pertence a UM Equipamento
+    public function equipamento()
+    {
+        return $this->belongsTo(Equipamento::class);
+    }
+
+    // O Usuário que criou a OS
+    public function criador()
+    {
+        return $this->belongsTo(User::class, 'user_id'); 
+    }
+
+    //O Usuário que atualizou a OS por último
+    public function atualizador()
+    {
+        // Ajuste o nome da chave estrangeira
+        return $this->belongsTo(User::class, 'user_id'); 
+    }
+
 }
