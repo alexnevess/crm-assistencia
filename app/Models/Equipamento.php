c0f51cf826a18d2d4abbfbdf861237c04d8921c8
alexnevess <alexsndsf@gmail.com>
2025-10-20 15:52:40 -0300

    Conclusão do model Equipamento

--- app/Models/Equipamento.php
+++ app/Models/Equipamento.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Equipamento extends Model
 {
-    //
+    protected $fillable = [
+        'empresa_id',
+        'cliente_id',
+        'equip_marca',      
+        'equip_modelo',     
+        'equip_numero_serie',
+    ];
+
+    //Um equipamento pertence a uma empresa
+    public function empresa()
+    {
+        return $this->belongsTo(Empresa::class);
+    }
+
+    //Um equipamento pertence a um cliente
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
+
+    //Um equipamento pode ter muitas ordens de serviço
+    public function ordemServico()
+    {
+        return $this->hasMany(OrdemServico::class);
+    }
 }
